3a3fce25c068ea8ff5d9ac1e1b541897fc9e9d4f
Jiawen Zhou <jz4112@ic.ac.uk>
2013-12-23 11:23:53 +0000

    implemented custom exception in scala

---
+++ src/main/scala/com/snowplowanalytics/forex/oerclient/UnavailableExchangeRateException.scala
@@ -0,0 +1,21 @@
+/* Copyright 2012 Demétrio de Castro Menezes Neto
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*/
+package com.snowplowanalytics.forex.oerclient;
+
+/**
+* throw UnavailableExchangeRateException 
+* exception when a forex rate cannot be found from API
+*/
+case class UnavailableExchangeRateException(msg: String) extends Exception
