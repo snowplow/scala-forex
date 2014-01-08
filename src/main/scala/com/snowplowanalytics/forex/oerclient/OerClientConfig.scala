812b9593ca225af4665cd3d77aaabcdfae99b5f4
Jiawen Zhou <jiawen.zhou0627@gmail.com>
2014-01-08 11:34:40 +0000

    issue #55 : move OerClientConfig into oerclient package done

---
+++ src/main/scala/com/snowplowanalytics/forex/oerclient/OerClientConfig.scala
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2013-2014 Snowplow Analytics Ltd. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package com.snowplowanalytics.forex
+package oerclient
+
+/**
+ * OER-specific configuration
+ */
+case class OerClientConfig(
+  appId: String,              // register an account on https://openexchangerates.org to obtain your unique key
+  configurableBase: Boolean  // boolean flag to identify the type of user,
+                              // i.e. if the user is entitled to configure the base currency
+)  
